f5cf4be481b24868f90f436fd562f7615e9db618
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-12 14:54:33 -0800

    added fecth-utils plan

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,4 @@
+// getBooks, getSingleBook(id), createBook, deleteBook(id)
+// getReviews, getSingleReview(id), createReview, deleteReview(id), updateReview(id, updatedReview)
+// getUser, checkAuth, redirectIfLoggedIn, SignUp, SignIn, Logout
+// createProfile, getProfile(id), getProfiles
