0b7027b0c277b9bc5ef3c611aba17c57d6c92a1f
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-13 17:22:55 -0800

    added plan to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { getUser } from './services/fetch-utils';
+import {
+  BrowserRouter as Router,
+  Switch,
+  NavLink,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
+  //track user's state
+  //add useEffect to user to fetch user, inject into userState on load
+  // handleLogout (call func, clear state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        {/* if user: render links (book-list, create, userProfile) */}
       </header>
+      <main>
+        {/* switch->route->ternary */}
+        {/* if user, redirect to bookList, if no call AuthPage */}
+        {/* if user, render bookList, if no call AuthPage */}
+        {/* if user, render bookDetail, if no call AuthPage */}
+        {/* if user, render Create, if no call AuthPage */}
+        {/* if user, render Profile, if no call AuthPage */}
+      </main>
     </div>
   );
 }
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,37 +1,36 @@
 import { client } from './client';
 // getUser, checkAuth, redirectIfLoggedIn, SignUp, SignIn, Logout
 export async function getUser() {
-    return client.auth.session();
+  return client.auth.session();
 }
 
 export async function checkAuth() {
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) location.replace('../');
+  if (!user) location.replace('../');
 }
 
 export async function redirectIfLoggedIn() {
-    if (await getUser()) {
-        location.replace('./books');
-    }
+  if (await getUser()) {
+    location.replace('./books');
+  }
 }
 
 export async function signUp(email, password) {
-    const response = await client.auth.signUp({ email, password });
+  const response = await client.auth.signUp({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
 export async function signIn(email, password) {
-    const response = await client.auth.signIn({ email, password });
+  const response = await client.auth.signIn({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
 export async function logout() {
-    await client.auth.signOut();
-
-    return window.location.href = '../';
+  await client.auth.signOut();
+  return window.location.href = '../';
 }
 
 // getBooks, getSingleBook(id), createBook, deleteBook(id)
